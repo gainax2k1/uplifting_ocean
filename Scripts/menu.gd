c60e6f1d9bbd08bb4dbd84d942c6604377052295
gainax2k1 <gainax2k1@pm.me>
2026-09-11 17:43:10 -0700

    added bgmusic

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,15 +1,9 @@
 extends Control
 
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/uplifting_ocean.tscn")
-	
+@onready var bg_music = %BGMusic
 
 
 
-func _on_sound_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
 
 
 
